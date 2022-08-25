360adf76ba83f4dba29b57bf1d322d1b5bd824b2
Vojtěch Vidra <vojtechvidra@gmail.com>
2022-08-25 08:39:33 +0200

    fix: noflash

--- src/noflash.js
+++ src/noflash.js
@@ -1,21 +1,22 @@
 (function () {
-  var classNameDark = 'dark-mode';
-  var classNameLight = 'light-mode';
+  var classNameDark = "dark-mode";
+  var classNameLight = "light-mode";
   function setClassOnDocument(darkMode) {
-    document.querySelector('html').classList.add(darkMode ? classNameDark : classNameLight);
-    document.querySelector('html').classList.remove(darkMode ? classNameLight : classNameDark);
+    document.querySelector("html").classList.add(darkMode ? classNameDark : classNameLight);
+    document.querySelector("html").classList.remove(darkMode ? classNameLight : classNameDark);
   }
 
-  var preferDarkQuery = '(prefers-color-scheme: dark)';
+  var preferDarkQuery = "(prefers-color-scheme: dark)";
   var mql = window.matchMedia(preferDarkQuery);
   var supportsColorSchemeQuery = mql.media === preferDarkQuery;
 
   var mode;
   try {
-    mode = localStorage.getItem('scheme-mode');
+    var modeValue = localStorage.getItem("scheme-mode");
+    if (modeValue) mode = JSON.parse(modeValue);
   } catch {}
 
-  if (supportsColorSchemeQuery && mode === 'system') setClassOnDocument(mql.matches);
-  else if (mode === 'light' || mode === 'dark') setClassOnDocument(modeValue === 'dark');
+  if (supportsColorSchemeQuery && mode === "system") setClassOnDocument(mql.matches);
+  else if (mode === "light" || mode === "dark") setClassOnDocument(mode === "dark");
   else setClassOnDocument(false);
 })();
