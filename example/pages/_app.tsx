bf5d9921458b802ff456a219493761e90c661e0f
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-05-22 15:17:04 +0200

    feat: Add example link

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { DarkModeProvider } from "@rbnd/react-dark-mode";
 import type { AppProps } from "next/app";
 
-import "@/styles/global.css";
-
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     // Wrap your app with <DarkModeProvider>
--- example/pages/_document.tsx
+++ example/pages/_document.tsx
@@ -1,16 +1,32 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Make sure your global styles and/or css variables are in the HTML file, not in an external CSS file.  */}
+        <style>{`
+          .light-mode {
+            color-scheme: light;
+            background-color: white;
+            color: black;
+          }
+          .dark-mode {
+            /* Don't forget the color-scheme css attribute. This will avoid light scrollbars in dark mode. */
+            color-scheme: dark;
+            background-color: black;
+            color: white;
+          }
+        `}</style>
+
+        {/* Noflash script in the <head> of the HTML document */}
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="/noflash.min.js" />
+      </Head>
       <body>
         <Main />
 
-        {/* Noflash script */}
-        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src="/noflash.min.js" />
 
         <NextScript />
       </body>
