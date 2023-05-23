0d845ffbb8c7b0a3596f37407b5b45376ddc7d96
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-05-23 06:37:16 +0200

    feat(example): Page title

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,8 +1,14 @@
 import { DarkModeControls } from "@/components/DarkModeControls";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>@rbnd/react-dark-mode demo</title>
+        <meta name="description" content="Demo of @rbnd/react-dark-mode npm package" />
+      </Head>
+
       <h1>@rbnd/react-dark-mode demo</h1>
       <p>
         This page is demo of `@rbnd/react-dark-mode` npm package. It showcases three different
